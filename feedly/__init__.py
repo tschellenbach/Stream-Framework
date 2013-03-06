2917ead0c7984e76c1d7e11d530e731a402b1238
Tommaso Barbugli <tbarbugli@gmail.com>
2013-03-06 12:17:04 +0100

    increase fan out batch size

--- feedly/__init__.py
+++ feedly/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
