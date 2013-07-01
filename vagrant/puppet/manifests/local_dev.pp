3ee774e1a6ad3a5d88066458d648f0da79e291f6
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-01 18:19:28 +0200

    fat and skinny vagrants

--- vagrant/puppet/manifests/local_dev.pp
+++ vagrant/puppet/manifests/local_dev.pp
@@ -6,7 +6,21 @@ include sudoers
 
 
 node default {
+    notice("Running with userdata $ec2_userdata and fqdn $fqdn")
+    $role = "local_dev"
+    # get the role from the userdata
+    if ($userdata) {
+      $role = $userdata['role']
+    }
+    notice("Running with role $role")
     include local_dev
+    
+      case $role {
+        # the main development setup, with example included
+        local_dev: { include local_dev }
+        # only cassandra
+        cassandra: { include cassandra_dev }
+      }
 }
 
 
