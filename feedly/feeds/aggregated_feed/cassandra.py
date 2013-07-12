534c85feb6366de6cbead7ae1430e7ef10289656
Tommaso Barbugli <tbarbugli@gmail.com>
2013-07-12 16:05:36 +0200

    add support for different feed classes in manager

--- feedly/feeds/aggregated_feed/cassandra.py
+++ feedly/feeds/aggregated_feed/cassandra.py
@@ -1,10 +1,12 @@
 from feedly.storage.cassandra import CassandraActivityStorage
 from feedly.storage.cassandra import CassandraTimelineStorage
 from feedly.feeds.aggregated_feed.base import AggregatedFeed
+from feedly.storage.utils.serializers.cassandra import ActivitySerializer
 from feedly.storage.utils.serializers.pickle_serializer import PickleSerializer
 
 
 class CassandraAggregatedFeed(AggregatedFeed):
+    activity_serializer = ActivitySerializer
+    activity_storage_class = CassandraActivityStorage
     timeline_serializer = PickleSerializer
     timeline_storage_class = CassandraTimelineStorage
-    activity_storage_class = CassandraActivityStorage
