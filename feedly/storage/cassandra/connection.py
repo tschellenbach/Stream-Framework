026db0e635f0c82e1b24884cb768d53b7fadfc0c
Tommaso Barbugli <tbarbugli@gmail.com>
2013-07-16 10:56:27 +0200

    use lots of connections for the pool

--- feedly/storage/cassandra/connection.py
+++ feedly/storage/cassandra/connection.py
@@ -4,7 +4,8 @@ from pycassa.pool import ConnectionPool
 def get_cassandra_connection(keyspace_name, hosts):
     if get_cassandra_connection._connection is None:
         get_cassandra_connection._connection = ConnectionPool(
-            keyspace_name, hosts)
+            keyspace_name, hosts, pool_size=len(hosts)*24,
+            prefill=False, timeout=10)
     return get_cassandra_connection._connection
 
 get_cassandra_connection._connection = None
