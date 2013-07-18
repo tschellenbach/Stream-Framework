f77a4382bad8344d58c1e313535f914344e447fb
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-18 10:41:14 +0200

    refactoring serialization

--- feedly/storage/utils/serializers/utils.py
+++
@@ -1,7 +0,0 @@
-from feedly.exceptions import SerializationException
-
-
-def check_reserved(value, reserved_characters):
-    if any([reserved in value for reserved in reserved_characters]):
-        raise SerializationException(
-            'encountered reserved character %s in %s' % (reserved, value))
