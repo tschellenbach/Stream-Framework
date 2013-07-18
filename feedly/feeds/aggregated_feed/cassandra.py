c46ca0f93c943f56a39c6a3bf9bb1c5925aea08c
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-18 12:38:09 +0200

    almost work

--- feedly/feeds/aggregated_feed/cassandra.py
+++ feedly/feeds/aggregated_feed/cassandra.py
@@ -1,5 +1,7 @@
 from feedly.feeds.aggregated_feed.base import AggregatedFeed
 from feedly.feeds.cassandra import CassandraFeed
+from feedly.serializers.cassandra.activity_serializer import \
+    CassandraActivitySerializer
 from feedly.serializers.pickle_serializer import \
     AggregatedActivityPickleSerializer
 from feedly.storage.cassandra.activity_storage import CassandraActivityStorage
@@ -7,7 +9,7 @@ from feedly.storage.cassandra.timeline_storage import CassandraTimelineStorage
 
 
 class CassandraAggregatedFeed(AggregatedFeed, CassandraFeed):
-    activity_serializer = CassandraActivityStorage
+    activity_serializer = CassandraActivitySerializer
     activity_storage_class = CassandraActivityStorage
     timeline_serializer = AggregatedActivityPickleSerializer
     timeline_storage_class = CassandraTimelineStorage
