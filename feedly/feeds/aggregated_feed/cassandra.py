1b0f1945da63c25377be34d5beede31aba98593f
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-23 11:50:35 +0200

    changed the default serializer

--- feedly/feeds/aggregated_feed/cassandra.py
+++ feedly/feeds/aggregated_feed/cassandra.py
@@ -1,9 +1,9 @@
 from feedly.feeds.aggregated_feed.base import AggregatedFeed
 from feedly.feeds.cassandra import CassandraFeed
+from feedly.serializers.aggregated_activity_serializer import \
+    AggregatedActivitySerializer
 from feedly.serializers.cassandra.activity_serializer import \
     CassandraActivitySerializer
-from feedly.serializers.pickle_serializer import \
-    AggregatedActivityPickleSerializer
 from feedly.storage.cassandra.activity_storage import CassandraActivityStorage
 from feedly.storage.cassandra.timeline_storage import CassandraTimelineStorage
 
@@ -11,5 +11,5 @@ from feedly.storage.cassandra.timeline_storage import CassandraTimelineStorage
 class CassandraAggregatedFeed(AggregatedFeed, CassandraFeed):
     activity_serializer = CassandraActivitySerializer
     activity_storage_class = CassandraActivityStorage
-    timeline_serializer = AggregatedActivityPickleSerializer
+    timeline_serializer = AggregatedActivitySerializer
     timeline_storage_class = CassandraTimelineStorage
