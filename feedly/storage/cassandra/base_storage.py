d6acfadb9d61460370be3397e93431867ced9d7d
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-24 12:03:30 +0200

    new version

--- feedly/storage/cassandra/base_storage.py
+++ feedly/storage/cassandra/base_storage.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 This is a thread local cache which works for, threads and greenlets
 '''
 local = Local()
-local.column_family_cache = dict()
 
 
 class CassandraBaseStorage(object):
@@ -22,7 +21,12 @@ class CassandraBaseStorage(object):
         '''
         Looks for the column family definition in the local cache
         '''
-        cache = local.column_family_cache
+        # setup the cache dict
+        cache = getattr(local, 'column_family_cache', None)
+        if cache is None:
+            cache = dict()
+            local.column_family_cache = cache
+        # and now use it to look for the column family
         cf = cache.get(self.column_family_name)
         if cf is None:
             logger.info('Retrieving ColumnFamily definition for %s', self.column_family_name)
