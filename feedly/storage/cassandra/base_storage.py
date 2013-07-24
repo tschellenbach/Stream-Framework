82cf900ab97dbd07c1c63130e17c6ead1dd7444e
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-24 11:55:57 +0200

    reenabled the smart caching"

--- feedly/storage/cassandra/base_storage.py
+++ feedly/storage/cassandra/base_storage.py
@@ -1,11 +1,14 @@
 from feedly.storage.cassandra.connection import get_cassandra_connection
 from pycassa.columnfamily import ColumnFamily
 from feedly.utils.local import Local
+import logging
 
+logger = logging.getLogger(__name__)
 '''
 This is a thread local cache which works for, threads and greenlets
 '''
 local = Local()
+local.column_family_cache = dict()
 
 
 class CassandraBaseStorage(object):
@@ -13,13 +16,18 @@ class CassandraBaseStorage(object):
     def __init__(self, keyspace_name, hosts, column_family_name, **kwargs):
         self.connection = get_cassandra_connection(keyspace_name, hosts)
         self.column_family_name = column_family_name
+        self.column_family = self.get_cached_column_family()
 
-    @property
-    def column_family(self):
-        cf = getattr(self, '_column_family', None)
+    def get_cached_column_family(self):
+        '''
+        Looks for the column family definition in the local cache
+        '''
+        cache = local.column_family_cache
+        cf = cache.get(self.column_family_name)
         if cf is None:
+            logger.info('Retrieving ColumnFamily definition for %s', self.column_family_name)
             cf = ColumnFamily(self.connection, self.column_family_name)
-            setattr(self, '_column_family', cf)
+            cache[self.column_family_name] = cf
         return cf
 
     def get_batch_interface(self):
