b1c15ac743815891ea86101c37f6af421b985357
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-07-25 15:46:43 +0200

    bit of cleanup

--- feedly/utils/timing.py
+++ feedly/utils/timing.py
@@ -7,21 +7,9 @@ class timer(object):
         self.total = 0.
         self.next()
 
-    def __iter__(self):
-        while True:
-            yield self.next()
-
     def next(self):
         times = self.times
         times.append(time.time())
         delta = times[-1] - times[-2]
         self.total += delta
         return delta
-
-    def get_avg(self, default=None):
-        if self.times:
-            return self.total / len(self.times)
-        else:
-            return default
-
-    avg = property(get_avg)
