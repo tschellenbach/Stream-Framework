c8126b58245a754d9dabe6d3a95877c2921be4e2
Tommaso Barbugli <tbarbugli@gmail.com>
2013-08-13 10:21:41 +0200

    bump version

--- feedly/__init__.py
+++ feedly/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
