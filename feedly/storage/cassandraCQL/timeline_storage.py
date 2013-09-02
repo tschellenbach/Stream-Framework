cc16d383506bad8b6f1d78911fb750151fd7b23a
Tommaso Barbugli <tbarbugli@gmail.com>
2013-09-02 18:22:16 +0200

    better modeling

--- feedly/storage/cassandraCQL/timeline_storage.py
+++ feedly/storage/cassandraCQL/timeline_storage.py
@@ -9,25 +9,30 @@ class CassandraTimelineStorage(BaseTimelineStorage):
     setup_connection()
 
     default_serializer_class = CassandraActivitySerializer
-    model = models.Activity
+    base_model = models.Activity
+
+    def __init__(self, serializer_class=None,  **options):
+        self.keyspace_name = options.pop('keyspace_name')
+        super(CassandraTimelineStorage, self).__init__(serializer_class, **options)
+        self.model = type('FeedModel', (self.base_model), {'__table_name__': self.keyspace_name})
 
     def contains(self, key, activity_id):
-        return self.model.objects.filter(user_id=key, activity_id=activity_id).count()
+        return self.model.objects.filter(feed_id=key, activity_id=activity_id).count()
 
     def index_of(self, key, activity_id):
         if not self.contains(key, activity_id):
             raise ValueError
-        return len(self.model.objects.filter(user_id=key, activity_id__gt=activity_id).values_list('user_id'))
+        return len(self.model.objects.filter(feed_id=key, activity_id__gt=activity_id).values_list('feed_id'))
 
     def get_nth_item(self, key, index):
-        return self.model.objects.filter(user_id=key).order_by('-activity_id')[index]
+        return self.model.objects.filter(feed_id=key).order_by('-activity_id')[index]
 
     def get_slice_from_storage(self, key, start, stop, pk_offset=False):
         '''
         :returns list: Returns a list with tuples of key,value pairs
         '''
         results = []
-        query = self.model.filter(user_id=key)
+        query = self.model.filter(feed_id=key)
         if pk_offset:
             query = query.filter(activity_id__lt=pk_offset)
         for activity in query.order_by('-activity_id')[start:stop]:
@@ -41,7 +46,7 @@ class CassandraTimelineStorage(BaseTimelineStorage):
         '''
         batch = batch_interface or BatchQuery()
         for model_instance in activities.itervalues():
-            model_instance.user_id = str(key)
+            model_instance.feed_id = str(key)
             model_instance.batch(batch).save()
         if batch_interface is None:
             batch.execute()
@@ -50,14 +55,14 @@ class CassandraTimelineStorage(BaseTimelineStorage):
         self.model.objects.filter(activity_id__in=activities.keys())
 
     def count(self, key, *args, **kwargs):
-        return self.model.objects.filter(user_id=key).count()
+        return self.model.objects.filter(feed_id=key).count()
 
     def delete(self, key, *args, **kwargs):
-        self.model.objects.filter(user_id=key).delete()
+        self.model.objects.filter(feed_id=key).delete()
 
     def trim(self, key, length, batch_interface=None):
         last_activity = self.get_slice_from_storage(key, 0, length)[-1]
         if last_activity:
             with BatchQuery():
-                for activity in self.model.filter(user_id=key, activity_id__lt=last_activity[0])[:1000]:
+                for activity in self.model.filter(feed_id=key, activity_id__lt=last_activity[0])[:1000]:
                     activity.delete()
