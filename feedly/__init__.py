2204dd1a2092d333d5fe026ab4eceefe480cb2c9
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-09-16 15:20:22 +0200

    bump

--- feedly/__init__.py
+++ feedly/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '0.8.118'
+__version__ = '0.8.119'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
