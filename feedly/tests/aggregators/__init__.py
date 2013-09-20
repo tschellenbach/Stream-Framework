9a7ac98180a23e7132790e4ef0a20d472cf555e6
Tommaso Barbugli <tbarbugli@gmail.com>
2013-09-20 16:02:28 +0200

    better test fixtures

--- feedly/tests/aggregators/__init__.py
+++ feedly/tests/aggregators/__init__.py
@@ -18,14 +18,14 @@ class AggregatorTest(unittest.TestCase):
         self.activity = FakeActivity(
             1, LoveVerb, 1, 1, datetime.datetime.now(), {})
         activities = []
-        for x in range(1, 11):
+        for x in range(2, 12):
             activity_time = datetime.datetime.now()
             find = x > 5
             activity = FakeActivity(
                 x, LoveVerb, 1, x, activity_time, dict(find=find))
             activities.append(activity)
         add_activities = []
-        for x in range(1, 26):
+        for x in range(12, 37):
             activity_time = datetime.datetime.now() + datetime.timedelta(
                 seconds=4)
             activity = FakeActivity(x, AddVerb, 1, x, activity_time, dict(x=x))
