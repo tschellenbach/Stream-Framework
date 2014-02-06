e27305fde023df04d98b83e16c821e4c0f210a16
Tommaso Barbugli <tbarbugli@gmail.com>
2014-02-06 20:40:42 +0100

    add import from string utility

--- feedly/utils/__init__.py
+++ feedly/utils/__init__.py
@@ -99,3 +99,26 @@ class memoized(object):
    def __get__(self, obj, objtype):
       '''Support instance methods.'''
       return functools.partial(self.__call__, obj)
+
+
+def get_class_from_string(path, default=None):
+    """
+    Return the class specified by the string.
+
+    """
+    try:
+        from importlib import import_module
+    except ImportError:
+        from django.utils.importlib import import_module
+    i = path.rfind('.')
+    module, attr = path[:i], path[i + 1:]
+    mod = import_module(module)
+    try:
+        return getattr(mod, attr)
+    except AttributeError:
+        if default:
+            return default
+        else:
+            raise ImportError(
+                'Cannot import name {} (from {})'.format(attr, mod))
+
