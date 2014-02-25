99d07c3167a057f5494329811d1ed353860e9b6e
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-02-25 11:49:13 +0100

    test case for custom feed

--- feedly/tests/feeds/aggregated_feed/redis.py
+++ feedly/tests/feeds/aggregated_feed/redis.py
@@ -1,6 +1,35 @@
 from feedly.feeds.aggregated_feed.redis import RedisAggregatedFeed
-from feedly.tests.feeds.aggregated_feed.base import TestAggregatedFeed
+from feedly.tests.feeds.aggregated_feed.base import TestAggregatedFeed,\
+    implementation
+from feedly.activity import AggregatedActivity
+from feedly.tests.feeds.redis import CustomActivity
+
+
+class CustomAggregated(AggregatedActivity):
+    pass
+
+
+class RedisCustomAggregatedFeed(RedisAggregatedFeed):
+    activity_class = CustomActivity
+    aggregated_activity_class = CustomAggregated
 
 
 class TestRedisAggregatedFeed(TestAggregatedFeed):
     feed_cls = RedisAggregatedFeed
+
+class TestRedisCustomAggregatedFeed(TestAggregatedFeed):
+    feed_cls = RedisCustomAggregatedFeed
+    activity_class = CustomActivity
+    aggregated_activity_class = CustomAggregated
+
+    @implementation
+    def test_custom_activity(self):
+        assert self.test_feed.count() == 0
+        self.feed_cls.insert_activity(
+            self.activity
+        )
+        self.test_feed.add(self.activity)
+        assert self.test_feed.count() == 1
+        results = self.test_feed[:10][0]
+        assert type(results[0]) == self.aggregated_activity_class
+        assert type(results[0].activities[0]) == self.activity_class
