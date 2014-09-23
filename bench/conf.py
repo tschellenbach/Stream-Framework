5ecf57a3289e9251843b2e157519d8418ce7dffa
Tommaso Barbugli <tbarbugli@gmail.com>
2014-09-23 08:51:10 +0200

    rename feedly

--- bench/conf.py
+++ bench/conf.py
@@ -1,8 +1,8 @@
 SECRET_KEY = '123456789'
 
-stream_framework_DEFAULT_KEYSPACE = 'test'
+STREAM_DEFAULT_KEYSPACE = 'test'
 
-stream_framework_CASSANDRA_HOSTS = [
+STREAM_CASSANDRA_HOSTS = [
     '127.0.0.1', '127.0.0.2', '127.0.0.3'
 ]
 
