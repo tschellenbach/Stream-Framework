4163746ea0385bb9a5981a4472f03392c21a8fc1
Tommaso Barbugli <tbarbugli@gmail.com>
2014-09-23 11:33:31 +0200

    prepare for next release

--- stream_framework/__init__.py
+++ stream_framework/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
