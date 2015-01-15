3c019a2bece2f3303bf6dd99644559694e0b6ff6
Tommaso Barbugli <tbarbugli@gmail.com>
2015-01-15 15:00:03 +0100

    update LRU memoizing for stream_framework too

--- stream_framework/__init__.py
+++ stream_framework/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
