85c547fa1e77a082ee6c05dabe993ec0ea471fcf
Tommaso Barbugli <tbarbugli@gmail.com>
2015-01-15 14:55:16 +0100

    release 1.1.4

--- feedly/__init__.py
+++ feedly/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '0.11.3'
+__version__ = '0.11.4'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
