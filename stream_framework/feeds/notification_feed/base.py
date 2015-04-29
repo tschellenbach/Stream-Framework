501487b085aa97e8059d61bd6e3f94272d67e177
Anislav Atanasov <anislav@gmail.com>
2015-04-29 23:10:48 +0300

    Define and extract ListsStorage API

--- stream_framework/feeds/notification_feed/base.py
+++ stream_framework/feeds/notification_feed/base.py
@@ -1,31 +1,14 @@
 from stream_framework.activity import NotificationActivity
 from stream_framework.feeds.aggregated_feed.base import AggregatedFeed
 from stream_framework.serializers.aggregated_activity_serializer import NotificationSerializer
+from stream_framework.storage.base_lists_storage import BaseListsStorage
+from stream_framework.utils.validate import validate_type_strict
 
-import random
 import logging
 logger = logging.getLogger(__name__)
 
 
-class BaseIdStorageClass(object):
-
-    def set(self, ids):
-        raise NotImplementedError()
-
-    def add(self, ids):
-        raise NotImplementedError()
-
-    def remove(self, ids):
-        raise NotImplementedError()
-
-    def __len__(self):
-        raise NotImplementedError()
-
-    def __iter__(self):
-        raise NotImplementedError()
-
-
-class NotificationFeed(AggregatedFeed):
+class BaseNotificationFeed(AggregatedFeed):
 
     key_format = 'notification_feed:%(user_id)s'
 
@@ -34,158 +17,137 @@ class NotificationFeed(AggregatedFeed):
     activity_storage_class = None
     activity_serializer = None
 
-    # : an optional storage used to keep track of unseen activity ids
-    unseen_ids_storage_class = None
-    # : an optional storage used to keep track of unread activity ids
-    unread_ids_storage_class = None
+    # : the storage class responsible to keep track of unseen/unread activity ids
+    counters_storage_class = None
+
+    # : define whether or not to keep track of unseen activity ids
+    track_unseen = True
+    # : define whether or not to keep track of unread activity ids
+    track_unread = True
 
     # : the max number of tracked unseen/unread activity ids
-    unseen_unread_max_length = 100
+    counters_max_length = 100
 
-    # : the chance to reduce the number of tracked unseen/unread activity ids
-    unseen_unread_trim_chance = 0.1
+    # : provides a part of the key used by counters_storage_class
+    counters_key_format = 'notification_feed:%(user_id)s'
 
     #: the key used for distributed locking
     lock_format = 'notification_feed:%(user_id)s:lock'
 
     def __init__(self, user_id):
-        super(NotificationFeed, self).__init__(user_id)
+        super(BaseNotificationFeed, self).__init__(user_id)
 
-        # TODO add validation that the storage classes inherit from BaseIdStorageClass
-        if self.unseen_ids_storage_class:
-            self.unseen_ids_storage = self.unseen_ids_storage_class()
-        if self.unread_ids_storage_class:
-            self.unread_ids_storage = self.unread_ids_storage_class()
-
-    def trim_unseen_unread_ids(self):
-        # trim the unseen/unread ids from time to time
-        if random.random() <= self.unseen_unread_trim_chance:
-            unseen_ids, unread_ids = self.get_unseen_unread_ids()
-            unseen_ids_to_remove, unread_ids_to_remove = None, None
-
-            if len(unseen_ids) > self.unseen_unread_max_length:
-                unseen_ids_to_remove = sorted(unseen_ids, reverse=True)[self.unseen_unread_max_length:]
-            if len(unread_ids) > self.unseen_unread_max_length:
-                unread_ids_to_remove = sorted(unread_ids, reverse=True)[self.unseen_unread_max_length:]
-
-            if unseen_ids_to_remove or unread_ids_to_remove:
-                self.update_unseen_unread_ids(unseen_ids=unseen_ids_to_remove,
-                                              unread_ids=unread_ids_to_remove,
-                                              operation='remove')
-
-    def update_unseen_unread_ids(self, unseen_ids=None, unread_ids=None, operation='set'):
-        if operation is ('set', 'add', 'remove'):
-            raise TypeError('%s is not supported' % operation)
-
-        # TODO perform the operation in a transaction
-        if self.unseen_ids_storage_class and unseen_ids:
-            func = getattr(self.unseen_ids_storage, operation)
-            func(unseen_ids)
-        if self.unread_ids_storage_class and unread_ids:
-            func = getattr(self.unread_ids_storage, operation)
-            func(unread_ids)
-
-        if operation == 'add':
-            self.trim_unseen_unread_ids()
-
-        # TODO fix this - in case an activity is added or removed the on_update_feed was already invoked
-        # TODO use a real-time transport layer to notify for these updates
-        self.on_update_feed([], [], self.get_notification_data())
-
-    def get_unseen_unread_ids(self):
-        '''
-        Returns two lists with activity ids.
-        The first one consists of ids of these aggregated activities that are not seen by the feed owner.
-        The second one consists of ids of these aggregated activities that are not read.
-        '''
-        unseen_ids = []
-        unread_ids = []
-
-        # TODO retrieve them in a batch
-        if self.unseen_ids_storage_class:
-            unseen_ids = list(self.unseen_ids_storage)
-        if self.unread_ids_storage_class:
-            unread_ids = list(self.unread_ids_storage)
-
-        return unseen_ids, unread_ids
-
-    def count_unseen_unread_ids(self):
-        '''
-        Counts the number of aggregated activities which are unseen and/or unread
-        '''
-        unseen_ids, unread_ids = self.get_unseen_unread_ids()
-        unseen_count = min(len(unseen_ids), self.unseen_unread_max_length)
-        unread_count = min(len(unread_ids), self.unseen_unread_max_length)
-        return unseen_count, unread_count
-
-    def acquire_lock(self, timeout=None):
-        '''
-        Provides a locking mechanism based on Redis that works within a distributed environment.
-
-        This method may be overridden in order to switch to a locking mechanism
-        provided by an alternative to Redis software like ZooKeeper.
-        '''
-        if not hasattr(self, 'lock_key'):
-            self.lock_key = self.lock_format % {'user_id': self.user_id}
-
-            from stream_framework.storage.redis.connection import get_redis_connection
-            self.redis = get_redis_connection()
-
-        return self.redis.lock(self.lock_key, timeout=timeout)
-
-    def get_activity_slice(self, start=None, stop=None, rehydrate=True):
-        activities = super(NotificationFeed, self).get_activity_slice(start, stop, rehydrate)
-        if activities and (self.unread_ids_storage_class or self.unseen_ids_storage):
-            unseen_ids, unread_ids = self.get_unseen_unread_ids()
-            for activity in activities:
-                if self.unread_ids_storage_class:
-                    activity.is_read = activity.serialization_id not in unread_ids
-                if self.unseen_ids_storage_class:
-                    activity.is_seen = activity.serialization_id not in unseen_ids
-        return activities
-
-    # TODO check whether remove_many needs to be overriden as well
-    def add_many(self, activities, **kwargs):
-        with self.acquire_lock(timeout=2):
-            current_activities = super(NotificationFeed, self).add_many(activities, **kwargs)
-            ids = [a.serialization_id for a in current_activities]
-            # TODO this looks correct, right?
-            self.update_unseen_unread_ids(ids, ids, operation='set')
-            return current_activities
-
-    def add_many_aggregated(self, aggregated, *args, **kwargs):
-        super(NotificationFeed, self).add_many_aggregated(aggregated, *args, **kwargs)
-        ids = [a.serialization_id for a in aggregated]
-        self.update_unseen_unread_ids(ids, ids, operation='add')
-
-    def remove_many_aggregated(self, aggregated, *args, **kwargs):
-        super(NotificationFeed, self).remove_many_aggregated(aggregated, *args, **kwargs)
-        ids = [a.serialization_id for a in aggregated]
-        self.update_unseen_unread_ids(ids, ids, operation='remove')
+        if self.counters_storage_class is None:
+            if self.track_unread or self.track_unseen:
+                raise ValueError('counters_storage_class must be set in case the unseen/unread activities are tracked')
+        else:
+            validate_type_strict(self.counters_storage_class, BaseListsStorage)
+            counters_key = self.counters_key_format % {'user_id': user_id}
+            self.feed_counters = self.counters_storage_class(key=counters_key,
+                                                             max_length=self.counters_max_length)
 
     def get_notification_data(self):
+        '''
+        Provides custom notification data that is used by the transport layer
+        when the feed is updated.
+        '''
         notification_data = dict()
-        unseen_count, unread_count = self.count_unseen_unread_ids()
 
-        if self.unseen_ids_storage_class:
+        if self.track_unseen and self.track_unread:
+            unseen_count, unread_count = self.feed_counters.count('unseen', 'unread')
             notification_data['unseen_count'] = unseen_count
-        if self.unread_ids_storage_class:
+            notification_data['unread_count'] = unread_count
+        elif self.track_unseen:
+            unseen_count = self.feed_counters.count('unseen')
+            notification_data['unseen_count'] = unseen_count
+        elif self.track_unread:
+            unread_count = self.feed_counters.count('unread')
             notification_data['unread_count'] = unread_count
 
         return notification_data
 
+    def update_counters(self, unseen_ids=None, unread_ids=None, operation='add'):
+        '''
+        Starts or stops tracking activities as unseen and/or unread.
+        '''
+        if self.counters_storage_class is not None:
+            if operation not in ('add', 'remove'):
+                raise TypeError('%s is not supported' % operation)
+
+            kwagrs = dict()
+            if unseen_ids is not None and self.track_unseen:
+                kwagrs['unseen'] = unseen_ids
+            if unread_ids is not None and self.track_unread:
+                kwagrs['unread'] = unread_ids
+
+            func = getattr(self.feed_counters, operation)
+            func(**kwagrs)
+
+            # TODO fix this - in case an activity is added or removed the on_update_feed was already invoked
+            # TODO use a real-time transport layer to notify for these updates
+            self.on_update_feed([], [], self.get_notification_data())
+
+    def get_activity_slice(self, start=None, stop=None, rehydrate=True):
+        '''
+        Retrieves a slice of activities and annotates them as read and/or seen.
+        '''
+        activities = super(BaseNotificationFeed, self).get_activity_slice(start, stop, rehydrate)
+        if activities and self.counters_storage_class is not None:
+
+            if self.track_unseen and self.track_unread:
+                unseen_ids, unread_ids = self.feed_counters.get('unseen', 'unread')
+            elif self.track_unseen:
+                unseen_ids = self.feed_counters.get('unseen')
+            elif self.track_unread:
+                unread_ids = self.feed_counters.get('unread')
+
+            for activity in activities:
+                if self.track_unseen:
+                    activity.is_seen = activity.serialization_id not in unseen_ids
+                if self.track_unread:
+                    activity.is_read = activity.serialization_id not in unread_ids
+
+        return activities
+
+    def add_many_aggregated(self, aggregated, *args, **kwargs):
+        '''
+        Adds the activities to the notification feed and marks them as unread/unseen.
+        '''
+        super(BaseNotificationFeed, self).add_many_aggregated(aggregated, *args, **kwargs)
+        ids = [a.serialization_id for a in aggregated]
+        self.update_counters(ids, ids, operation='add')
+
+    def remove_many_aggregated(self, aggregated, *args, **kwargs):
+        '''
+        Removes the activities from the notification feed and marks them as read/seen.
+        '''
+        super(BaseNotificationFeed, self).remove_many_aggregated(aggregated, *args, **kwargs)
+        ids = [a.serialization_id for a in aggregated]
+        self.update_counters(ids, ids, operation='remove')
+
     def mark_activity(self, activity_id, seen=True, read=False):
+        '''
+        Marks the given activity as seen or read or both.
+        '''
         self.mark_activities([activity_id], seen, read)
 
     def mark_activities(self, activity_ids, seen=True, read=False):
+        '''
+        Marks all of the given activities as seen or read or both.
+        '''
         unseen_ids = activity_ids if seen else []
         unread_ids = activity_ids if read else []
-        self.update_unseen_unread_ids(unseen_ids=unseen_ids,
-                                      unread_ids=unread_ids,
-                                      operation='remove')
+        self.update_counters(unseen_ids=unseen_ids,
+                             unread_ids=unread_ids,
+                             operation='remove')
 
     def mark_all(self, seen=True, read=False):
         '''
-        Mark all the entries as seen or read
+        Marks all of the given activities as seen or read or both.
         '''
-        self.update_unseen_unread_ids(unseen=[], unread=[], operation='set')
+        args = []
+        if seen and self.track_unseen:
+            args.append('unseen')
+        if read and self.track_unread:
+            args.append('unread')
+        self.feed_counters.flush(*args)
